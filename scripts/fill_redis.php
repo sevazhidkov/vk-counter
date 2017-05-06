eb3651452beabcce42382e389ad399ccf98eaedd
Seva Zhidkov <zhidkovseva@gmail.com>
2017-05-07 00:33:54 +0300

    add stats

--- scripts/fill_redis.php
+++ scripts/fill_redis.php
@@ -11,8 +11,13 @@ if ($argc !== 3) {
 
 $key = $argv[1];
 $amount = intval($argv[2]);
+$stats_sum = 0.0; // Store sum of times of execution for each push.
 
 $current_time = time();
 for ($i = 0; $i < $amount; $i++) {
+  $start = microtime(true);
   $redis_client->rpush($key, $current_time);
+  $stats_sum += microtime(true) - $start;
 }
+
+echo "Average execution time per RPUSH:\n" + strval($stats_sum / $amount);
--- scripts/fill_redis_expired.php
+++ scripts/fill_redis_expired.php
@@ -11,9 +11,14 @@ if ($argc !== 3) {
 
 $key = $argv[1];
 $amount = intval($argv[2]);
+$stats_sum = 0.0; // Store sum of times of execution for each push.
 
 $current_time = time();
 $expired_time = $current_time - (60 * 60 * 24 + 1);
 for ($i = 0; $i < $amount; $i++) {
+  $start = microtime(true);
   $redis_client->rpush($key, $expired_time);
+  $stats_sum += microtime(true) - $start;
 }
+
+echo "Average execution time per RPUSH:\n" + strval($stats_sum / $amount);
