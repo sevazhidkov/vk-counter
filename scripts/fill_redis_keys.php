3999995dc6da75565979ac0a8f4a5ac4434c4fbe
Seva Zhidkov <zhidkovseva@gmail.com>
2017-05-09 12:21:05 +0400

    add script for geterating many redis keys

---
+++ scripts/fill_redis_keys.php
@@ -0,0 +1,23 @@
+<?php
+require('vendor/autoload.php');
+
+$redis_url = getenv('REDIS_URL');
+$redis_client = new Predis\Client($redis_url);
+
+if ($argc !== 3) {
+    echo "Usage: php scripts/fill_redis_keys.php [keys].\n";
+    exit(1);
+}
+
+$keys = $argv[1];
+$stats_sum = 0.0; // Store sum of times of execution for each push.
+
+$current_time = time();
+for ($i = 0; $i < $keys; $i++) {
+  $start = microtime(true);
+  $key = 'key' . $i
+  $redis_client->rpush($key, $current_time);
+  $stats_sum += microtime(true) - $start;
+}
+
+echo "Average execution time per RPUSH:\n" + strval($stats_sum / $keys);
